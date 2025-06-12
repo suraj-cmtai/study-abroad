79caa9d41b61dd88952164c72accdfc0bf2e7eb6
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2025-06-12 18:52:32 +0530

    fix

--- app/api/config/firebase.ts
+++ app/api/config/firebase.ts
@@ -3,9 +3,6 @@ import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
-// Load environment variables
-
-require("dotenv").config();
 
 // Load Firebase service account credentials
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || "{}");
