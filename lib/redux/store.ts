d1066d1ddc6d9f42637b29701cafb48533a9f7a9
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2025-06-23 20:55:00 +0530

    cleanup

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -1,13 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import galleryReducer from "./features/gallerySlice";
-import leadReducer from "./features/leadSlice";
-import locationReducer from "./features/locationSlice";
-import packageReducer from "./features/packageSlice";
-import testimonialReducer from "./features/testimonialSlice";
-import planReducer from "./features/planSlice";
 import authReducer from "./features/authSlice";
-import countReducer from "./features/countSlice";
-import userReducer from "./features/userSlice";
 import blogReducer from "./features/blogSlice";
 import courseReducer from "./features/courseSlice";
 import subscriberReducer from "./features/subscriberSlice";
@@ -17,14 +10,7 @@ import contactReducer from "./features/contactSlice";
 export const store = configureStore({
   reducer: {
     gallery: galleryReducer,
-    lead: leadReducer,
-    location: locationReducer,
-    package: packageReducer,
-    testimonial: testimonialReducer,
-    plan: planReducer,
     auth: authReducer,
-    count: countReducer,
-    user: userReducer,
     blog: blogReducer,
     course: courseReducer,    subscriber: subscriberReducer,
     contact: contactReducer,
