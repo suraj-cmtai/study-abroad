e892d52f69a8d5018982e70947819ff36ee1b343
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2025-07-26 13:09:12 +0530

    live chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,23 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} antialiased`}>
         <ReduxProvider>{children}</ReduxProvider>
+        {/* Start of Tawk.to Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/6884843c828524191a31a416/1j12q93p9';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
+        {/* End of Tawk.to Script */}
       </body>
     </html>
   );
